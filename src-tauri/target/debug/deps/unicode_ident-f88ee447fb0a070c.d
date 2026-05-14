ff826d2850759f53e84f43afaf3f619f2924f689
Vickey Kapoor <vickey.kapoor03@gmail.com>
2026-05-14 10:22:06 -0500

    Chore: gitignore src-tauri/target build artifacts

--- src-tauri/target/debug/deps/unicode_ident-f88ee447fb0a070c.d
+++
@@ -1,8 +0,0 @@
-C:\Users\vicke\GitHub\Operon\src-tauri\target\debug\deps\unicode_ident-f88ee447fb0a070c.d: C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\vicke\GitHub\Operon\src-tauri\target\debug\deps\libunicode_ident-f88ee447fb0a070c.rlib: C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\vicke\GitHub\Operon\src-tauri\target\debug\deps\libunicode_ident-f88ee447fb0a070c.rmeta: C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\Users\vicke\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
